e9303d89f04536b86587b3fd8b108061ffbe86e8
Lars Trieloff <trieloff@adobe.com>
2022-02-14 09:41:03 +0100

    feat(search): add auto-complete
    based on datalist elements, simple

--- blocks/asset-search/asset-search.js
+++ blocks/asset-search/asset-search.js
@@ -7,12 +7,37 @@ export default function decorate(block) {
     textfield.value = new URL(window.location.href).searchParams.get('q');
   }
 
+  const datalist = document.createElement('datalist');
+  datalist.id = 'query-suggestions';
+
   // update the URL when the input changes
   textfield.addEventListener('input', () => {
     const myurl = new URL(window.location.href);
     myurl.searchParams.set('q', textfield.value);
     window.changeURLState({ query: textfield.value }, myurl.href);
+
+    const query = myurl.searchParams.get('q');
+
+    const url = new URL('https://SWFXY1CU7X-dsn.algolia.net/1/indexes/assets_query_suggestions');
+    url.searchParams.set('query', query);
+    url.searchParams.set('x-algolia-api-key', 'bd35440a1d9feb709a052226f1aa70d8');
+    url.searchParams.set('x-algolia-application-id', 'SWFXY1CU7X');
+
+    fetch(url.href).then(async (res) => {
+      const { hits } = await res.json();
+      if (hits.length) {
+        datalist.innerHTML = '';
+      }
+      hits.forEach((hit) => {
+        const option = document.createElement('option');
+        option.innerHTML = hit.query;
+        datalist.append(option);
+      });
+      // enable autocompletion if there are options
+      textfield.setAttribute('list', datalist.id);
+    });
   });
 
   block.replaceWith(textfield);
+  textfield.after(datalist);
 }
