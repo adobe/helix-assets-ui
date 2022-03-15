36ffe8a243a0313eedaadf89d82b596e2b92f4af
Lars Trieloff <trieloff@adobe.com>
2022-03-15 14:43:34 +0100

    feat(results): allow selection of the source index using the `index` URL parameter
    this supports the multiple indexes added in https://github.com/adobe/helix-asset-ingestor/pull/22

--- blocks/asset-results/asset-results.js
+++ blocks/asset-results/asset-results.js
@@ -139,12 +139,13 @@ export default function decorate(block) {
   const search = () => {
     const myurl = new URL(window.location.href);
     const query = myurl.searchParams.get('q');
+    const index = myurl.searchParams.get('index') || 'assets';
 
     const terms = query.split(' ');
     const filters = terms.filter((term) => term.match(':')).join(' AND ');
     const words = terms.filter((term) => !term.match(':')).join(' ');
 
-    const url = new URL('https://SWFXY1CU7X-dsn.algolia.net/1/indexes/assets');
+    const url = new URL('https://SWFXY1CU7X-dsn.algolia.net/1/indexes/' + index);
     url.searchParams.set('query', words);
     url.searchParams.set('x-algolia-api-key', 'bd35440a1d9feb709a052226f1aa70d8');
     url.searchParams.set('x-algolia-application-id', 'SWFXY1CU7X');
