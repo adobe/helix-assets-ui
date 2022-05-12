1a7a69a159f3dfef21d8a8ae0a494bcf7db3f64e
pkoch73 <35709927+pkoch73@users.noreply.github.com>
2022-05-12 13:04:04 -0400

    Update asset-results.js

--- blocks/asset-results/asset-results.js
+++ blocks/asset-results/asset-results.js
@@ -237,17 +237,20 @@ export default function decorate(block) {
     });
     const pictureDiv = modal.querySelector('.asset-results-oneup-picture');
     const moreDiv = modal.querySelector('.asset-results-oneup-more');
-    console.log('Asset:', asset);
-    const assetDescription = asset.alt != undefined ? asset.alt : asset.caption;
-    const createdDate = asset.created != undefined ?  new Date(asset.created).toLocaleDateString() : 'N/A';
-    const modDate = asset.modified != undefined ?  new Date(asset.modified).toLocaleDateString() : 'N/A';
+    
+    var assetDescription, createdDate, modDate
+    if (asset) {
+      assetDescription = asset.alt != undefined ? asset.alt : asset.caption;
+      createdDate = asset.created != undefined ?  new Date(asset.created).toLocaleDateString() : 'N/A';
+      modDate = asset.modified != undefined ?  new Date(asset.modified).toLocaleDateString() : 'N/A';
+    }
     const displayNameMap = {
       rum: "Website",
       stock: "Stock",
       brandportal: "Brand Portal",
       aem: "AEM"
     };
-    console.log(asset);
+
     const infoConfig = [{
       title: 'Information',
       infos: [
